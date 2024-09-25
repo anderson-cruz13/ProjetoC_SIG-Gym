b6461b8815f241f25cdca2f52b0b84c46edfcf98
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-25 18:38:10 -0300

    feat: loop principal do main para transição

--- main.c
+++ main.c
@@ -1,14 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "view/interface.c"
 
+void limparTela(void){
+    system("clear");
+}
+
+void gerenciarTelas(char opcao){
+    switch (opcao){
+    limparTela();
+    case '1':
+        telaCadastro();
+        break;
+    case '2':
+        telaPesquisar();
+        break;
+    case '3':
+        telaAtualizar();
+        break;
+    case '4':
+        telaDeletar();
+        break;
+    case '5':
+        telaRelatorios();
+        break;
+    case '6':
+        telaInfo();
+        break;
+    default:
+        break;
+    }
+}
+
 int main(void){
-    telaPrincipal();
-    telaCadastro();
-    telaPesquisar();
-    telaAtualizar();
-    telaDeletar();
-    telaRelatorios();
-    telaInfo();
+    char opcao;
+
+    do {
+        limparTela();
+        opcao = telaPrincipal();
+
+        if(opcao == '0'){
+            printf("Saindo...\n");
+            break;
+        } else if(opcao == '1' || opcao == '2' || opcao == '3' 
+            || opcao == '4' || opcao == '5' || opcao == '6'){
+            gerenciarTelas(opcao);
+        }
+
+    } while(1);
 
     return 0;
 }
