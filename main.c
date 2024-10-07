f171cd58148412e76296bdc55856ecdde0682ea6
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-07 10:01:10 -0300

    feat: update de loop principal

--- main.c
+++ main.c
@@ -1,35 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "view/interface.h"
 #include "model/cliente.h"
+#include "model/equipamentos.h"
+#include "model/funcionario.h"
+#include "model/treino.h"
+#include "controller/entradas.h"
+#include "controller/loops.h"
 
-void limparBuffer(void){
-    int buffer;
-    while((buffer = getchar()) != '\n' && buffer != EOF);
-}
 
 int main(void){
-    telaPrincipal();
-    limparBuffer();
-    telaCadastro();
-    limparBuffer();
-    cadastrarCliente();
-    limparBuffer();
-    telaPesquisar();
-    limparBuffer();
-    pesquisarCliente();
-    limparBuffer();
-    telaAtualizar();
-    limparBuffer();
-    atualizarCliente();
-    limparBuffer();
-    telaDeletar();
-    limparBuffer();
-    deletarCliente();
-    limparBuffer();
-    telaRelatorios();
-    limparBuffer();
-    telaInfo();
-    limparBuffer();
+    char opMain;
+
+    do{
+
+        telaPrincipal();
+        opMain = lerOpcao();
+
+        switch (opMain){
+            case '1':
+                loopCadastrar();
+                break;
+            case '2':
+                loopPesquisar();
+                break;
+
+            case '3':
+                loopAtualizar();
+                break;
+
+            case '4':
+                loopDeletar();
+                break;
+
+            case '5':
+                loopRelatorios();
+                break;
+
+            case '6':
+                telaInfo();
+                pausarTela();
+                break;
+
+            case '0':
+                printf("Obrigado por usar o programa. :)\n");
+                opMain = '0';
+                break;
+
+            default:
+                printf("Opção inválida!\n");
+                pausarTela();
+                break;
+        }
+
+    } while (opMain != '0');
 
     return 0;
 }
