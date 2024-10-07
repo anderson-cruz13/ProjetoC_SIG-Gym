ffa71a635e003a5783a5ede355515d8a9d096779
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-07 10:00:54 -0300

    feat: add files em models (SIG-Gym)

---
+++ model/equipamentos.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cadastrarEquipamento(void){
+    system("clear");
+    printf("|_________________________________________________|\n");
+    printf("|.....  .  ... CADASTRAR EQUIPAMENTO ...  .  .....|\n");
+    printf(" _________________________________________________|\n");
+    printf("> Nome: \n");
+    printf("> Função: \n");
+    printf("> Marca: \n");
+    printf("> Preço: \n");
+    printf("> Fabricante: \n");
+    printf("> Quantidade: \n");
+    printf("> Código de barras: \n");
+    printf("|________________________________________________|\n");
+}
+
+void pesquisarEquipamento(void){
+    system("clear");
+    printf("|_______________________________________________________________|\n");
+    printf("|.....  .  ... . .... PESQUISAR EQUIPAMENTO .... . ...  .  .....|\n");
+    printf(" _______________________________________________________________|\n");
+    printf("\n>Digite o Código de barras do equipamento (apenas números): \n");
+    printf("|_______________________________________________________________|\n");
+}
+
+void atualizarEquipamento(void){
+    system("clear");
+    printf("|_______________________________________________________________|\n");
+    printf("|.....  .  ... . .... ATUALIZAR EQUIPAMENTO .... . ...  .  .....|\n");
+    printf("|_______________________________________________________________|\n");
+    printf("\n>Digite o Código de barras do equipamento (apenas números): \n");
+    printf("|_______________________________________________________________|\n");
+}
+
+void deletarEquipamento(void){
+    system("clear");
+    printf("|_____________________________________________________________|\n");
+    printf("|.....  .  ... . .... DELETAR EQUIPAMENTO .... . ...  .  .....|\n");
+    printf("|_____________________________________________________________|\n");
+    printf("\n>Digite o Código de barras do equipamento (apenas números): \n");
+    printf("|_____________________________________________________________|\n");
+}
