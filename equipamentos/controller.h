ee6330b06ff2953a3e89086fc9ad9a20d23939d1
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-24 17:33:36 -0300

    refactor: MVC do módulo de equipamentos

---
+++ equipamentos/controller.h
@@ -0,0 +1,9 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+void cadastrarEquipamento(void);
+void pesquisarEquipamento(void);
+void atualizarEquipamento(void);
+void deletarEquipamento(void);
+
+#endif // CONTROLLER_H
